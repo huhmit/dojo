767db4669c9f169b1717ec51a8634ff67f94cbb5
Cameron Watson <charltonc98@gmail.com>
2021-06-01 16:47:05 +0100

    added SelectedText dependency property
    thanks to https://github.com/thomaslevesque

---
+++ DependencyProperties/TextBoxHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Dojo.DependencyProperties
+{
+    public static class TextBoxHelper
+    {
+
+        public static string GetSelectedText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(SelectedTextProperty);
+        }
+
+        public static void SetSelectedText(DependencyObject obj, string value)
+        {
+            obj.SetValue(SelectedTextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedTextProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectedText",
+                typeof(string),
+                typeof(TextBoxHelper),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, SelectedTextChanged));
+
+        private static void SelectedTextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is TextBox tb)
+            {
+                if (e.OldValue == null && e.NewValue != null)
+                {
+                    tb.SelectionChanged += tb_SelectionChanged;
+                }
+                else if (e.OldValue != null && e.NewValue == null)
+                {
+                    tb.SelectionChanged -= tb_SelectionChanged;
+                }
+
+                if (e.NewValue is string newValue && newValue != tb.SelectedText)
+                {
+                    tb.SelectedText = newValue;
+                }
+            }
+        }
+
+        static void tb_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                SetSelectedText(tb, tb.SelectedText);
+            }
+        }
+
+    }
+}
