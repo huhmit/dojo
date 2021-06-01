a9f89d6ae4a14b053f5a12a7806627f244a3dffd
Cameron Watson <charltonc98@gmail.com>
2021-06-01 09:27:41 +0100

    added view models

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,86 @@
+﻿namespace Dojo.ViewModels
+{
+    using Markdig;
+    using Markdig.Wpf;
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Xaml;
+    using XamlReader = System.Windows.Markup.XamlReader;
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private FlowDocument _markdownDocument;
+        private string _plainText;
+
+        private ICommand _boldCommand;
+
+        public FlowDocument MarkdownDocument
+        {
+            get
+            {
+                return _markdownDocument;
+            }
+            set
+            {
+                _markdownDocument = value;
+                NotifyPropertyChanged(nameof(MarkdownDocument));
+            }
+        }
+
+        public string PlainText
+        {
+            get
+            {
+                return _plainText;
+            }
+            set
+            {
+                _plainText = value;
+                OnLoaded();
+                NotifyPropertyChanged(nameof(PlainText));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            MarkdownDocument = new FlowDocument();
+            PlainText = File.ReadAllText(@"C:\Users\charlton.cameron\Desktop\todo.md");
+        }
+
+        private static MarkdownPipeline BuildPipeline()
+        {
+            return new MarkdownPipelineBuilder()
+                .UseSupportedExtensions()
+                .Build();
+        }
+
+        private void OnLoaded()
+        {
+            var markdown = PlainText;
+            var xaml = Markdig.Wpf.Markdown.ToXaml(markdown, BuildPipeline());
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xaml));
+            using var reader = new XamlXmlReader(stream, new MyXamlSchemaContext());
+            if (XamlReader.Load(reader) is FlowDocument document)
+            {
+                MarkdownDocument = document;
+            }
+        }
+
+        internal class MyXamlSchemaContext : XamlSchemaContext
+        {
+            public override bool TryGetCompatibleXamlNamespace(string xamlNamespace, out string compatibleNamespace)
+            {
+                if (xamlNamespace.Equals("clr-namespace:Markdig.Wpf", StringComparison.Ordinal))
+                {
+                    compatibleNamespace = $"clr-namespace:Markdig.Wpf;assembly={Assembly.GetAssembly(typeof(Styles)).FullName}";
+                    return true;
+                }
+                return base.TryGetCompatibleXamlNamespace(xamlNamespace, out compatibleNamespace);
+            }
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Public Properties
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+
+        #endregion Public Properties
+
+        #region Protected Methods
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion Protected Methods
+
+        #region Public Methods
+
+        public static void NotifyStaticPropertyChanged(string PropertyName)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        #endregion Public Methods
+    }
+}
